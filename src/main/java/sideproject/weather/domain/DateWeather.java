79213a890dea249be08c039d745477c039b512a1
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-10-24 14:58:30 +0900

    보완해야 할 부분 수정작업

--- src/main/java/sideproject/weather/domain/DateWeather.java
+++ src/main/java/sideproject/weather/domain/DateWeather.java
@@ -8,11 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity(name = "date_weather")
 @Getter
 @Setter
-@Entity(name = "date_weather")
 @NoArgsConstructor
 public class DateWeather {
+
     @Id
     private LocalDate date;
     private String weather;
