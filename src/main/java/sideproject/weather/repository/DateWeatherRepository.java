715f0feef2e6e3f2aa220461a409d2e7a2814c89
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2023-10-24 14:58:58 +0900

    보완해야 할 부분 수정작업

--- src/main/java/sideproject/weather/repository/DateWeatherRepository.java
+++ src/main/java/sideproject/weather/repository/DateWeatherRepository.java
@@ -1,11 +1,13 @@
 package sideproject.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sideproject.weather.domain.DateWeather;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
     List<DateWeather> findAllByDate(LocalDate localDate);
 }
